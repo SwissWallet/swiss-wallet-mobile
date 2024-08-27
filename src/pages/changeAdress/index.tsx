9e63da479808d0b36f89146cfa607c483c46b4d7
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-26 21:13:53 -0300

    feat:added load in the page

--- src/pages/changeAdress/index.tsx
+++ src/pages/changeAdress/index.tsx
@@ -3,7 +3,7 @@ import { View,Box,Text,Input, HStack} from "@gluestack-ui/themed";
 import HeaderWithoutPoints from "../../components/HeaderWithoutPoints";
 import Title from "../../components/Title";
 import { InputField } from "@gluestack-ui/themed";
-import { Alert, TouchableOpacity } from "react-native";
+import { ActivityIndicator, Alert, Keyboard, Modal, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 import DropShadow from "react-native-drop-shadow";
 import api from "../../service/api";
 
@@ -20,8 +20,12 @@ function ChangeAdress():JSX.Element{
     const [city, setCity] = useState("")
     const [uf, setUf] = useState("");
     const [logradouro, setLogradouro] = useState(""); 
+
+    const [loading, setLoading] = useState(false);
+
    
     const handleLocation = (text:string) => {
+        setLoading(true);
         setCep(text);
         fetch(`https://viacep.com.br/ws/${text}/json/`)
         .then(res => res.json())
@@ -29,6 +33,7 @@ function ChangeAdress():JSX.Element{
             setCity(json.localidade);
             setLogradouro(json.logradouro);
             setUf(json.uf);
+            setLoading(false);
         });
     }
 
@@ -62,8 +67,8 @@ function ChangeAdress():JSX.Element{
             console.log(json.data);
             return Alert.alert("Endereço alterado com sucesso!")
         }).catch((error) =>{
-            console.log(error);
 
+            console.log(error);
             return Alert.alert("Algo inesperado aconteceu!")
 
         })
@@ -71,59 +76,66 @@ function ChangeAdress():JSX.Element{
 
     return(
 
+        <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
+            <View>     
+                <HeaderWithoutPoints />
+                    
+                <Title name="Alterar endereço"></Title>
 
-        <View>     
-            <HeaderWithoutPoints />
+                <Box>
+
+                    <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={20}>CEP</Text>
+                    <Input   mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={365}>
+                        <InputField color="#000" fontWeight="bold" onChangeText={(text) => setCep(text)}/>
+                    </Input>    
+
+                    <HStack alignItems="center" >
+                        <Box>
+                            <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={30}>Cidade</Text>
+                            <Input isDisabled mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={180}>
+                                    <InputField color="#000" fontWeight="bold" value={city} />
+                            </Input>
+                        </Box>
+
+                        <Box>
+                            <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={30}>UF</Text>
+                            <Input  isDisabled mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={150}>
+                                    <InputField color="#000" fontWeight="bold" value={uf}/>
+                            </Input>
+                        </Box>
+                    </HStack>
+
+
+                    <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={20}>Logradouro</Text>
+                    <Input isDisabled mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={365}>
+                            <InputField color="#000" fontWeight="bold" value={logradouro} />
+                    </Input>
+
+                    <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={20}>Número</Text>
+                    <Input mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={365}>
+                            <InputField color="#000" fontWeight="bold" onChangeText={(text) => setNumber(text)}/>
+                    </Input>
                 
-            <Title name="Alterar endereço"></Title>
+                </Box>
 
-            <Box>
-
-                <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={20}>CEP</Text>
-                <Input   mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={365}>
-                    <InputField color="#000" fontWeight="bold" onChangeText={(text) => setCep(text)}/>
-                </Input>    
-
-                <HStack alignItems="center" >
-                    <Box>
-                        <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={30}>Cidade</Text>
-                        <Input isDisabled mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={180}>
-                                <InputField color="#000" fontWeight="bold" value={city} />
-                        </Input>
-                    </Box>
-
-                    <Box>
-                        <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={30}>UF</Text>
-                        <Input  isDisabled mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={150}>
-                                <InputField color="#000" fontWeight="bold" value={uf}/>
-                        </Input>
-                    </Box>
-                </HStack>
+                <Box>
+                    <TouchableOpacity onPress={validationInputs}>
+                        <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 4, height: 5}, shadowRadius: 2, shadowOpacity: 0.5}}>
+                            <Box ml={22} mr={22} bgColor="#9A1915" mt={45} alignItems="center" justifyContent="center" height={45} borderRadius={10} width={365}>
+                                <Text color="#fff" fontWeight="$bold">Confirmar</Text>
+                            </Box> 
+                        </DropShadow>
+                    </TouchableOpacity>
+                </Box>
 
 
-                <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={20}>Logradouro</Text>
-                <Input isDisabled mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={365}>
-                        <InputField color="#000" fontWeight="bold" value={logradouro} />
-                </Input>
-
-                <Text fontWeight="$bold" color="#000" fontSize={17}  marginLeft={30} mt={20}>Número</Text>
-                <Input mt={7}  marginLeft={30} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" width={365}>
-                        <InputField color="#000" fontWeight="bold" onChangeText={(text) => setNumber(text)}/>
-                </Input>
-               
-            </Box>
-
-            <Box>
-                <TouchableOpacity onPress={validationInputs}>
-                    <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 4, height: 5}, shadowRadius: 2, shadowOpacity: 0.5}}>
-                        <Box ml={22} mr={22} bgColor="#9A1915" mt={45} alignItems="center" justifyContent="center" height={45} borderRadius={10} width={365}>
-                            <Text color="#fff" fontWeight="$bold">Confirmar</Text>
-                        </Box> 
-                    </DropShadow>
-                </TouchableOpacity>
-            </Box>
-
-        </View>
+                <Modal transparent visible={loading}>
+                    <View flex={1} justifyContent="center" alignItems="center">
+                        <ActivityIndicator size="large" color="#000"/>
+                    </View>
+                </Modal>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
