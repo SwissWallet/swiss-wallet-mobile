347a7d236f1f1cfa3b5e5ee26291e31f9f7a939c
henriquepablo <pablo269704@gmail.com>
2024-09-04 08:11:08 -0300

    feat:component list card finalized

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Text, View } from "@gluestack-ui/themed";
+import { Box, Text, View } from "@gluestack-ui/themed";
 import HeaderWithPoints from "../../components/HeaderWithPoints";
+import ListProductCard from "../../components/ListProductCard";
+import DropShadow from "react-native-drop-shadow";
+import { ScrollView } from "react-native";
 
 function Store():JSX.Element{
     return(
-        <View>
-            <HeaderWithPoints/>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View>
+                <HeaderWithPoints/>
+
+                <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
+
+                    <Box mt={70}>
+                        <ListProductCard categoria="STORE"/>    
+                    </Box>                    
+
+                    
+                </DropShadow>
+            </View>
+        </ScrollView>
     );
 }
 
