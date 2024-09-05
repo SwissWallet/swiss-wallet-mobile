5d49ad04ce55bf1fa983619570c3577c0aeefebf
Milena-MM <milenamelo1709@gmail.com>
2024-09-04 21:04:46 -0300

    feat: consumindo a api e renderizando as imagens em cada categoria

--- src/pages/Coffe/index.tsx
+++ src/pages/Coffe/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { Text, View } from "@gluestack-ui/themed";
+import { Box, Text, View } from "@gluestack-ui/themed";
 import HeaderWithPoints from "../../components/HeaderWithPoints";
+import DropShadow from "react-native-drop-shadow";
+import ListProductCard from "../../components/ListProductCard";
 
 function Coffe():JSX.Element{
     return(
         <View>
             <HeaderWithPoints/>
+            <Box mb={-50} ml={22} mt={45}>
+                        <Text color="$black" fontWeight={"$bold"} fontSize={40} >
+                            Cantina
+                        </Text>
+                        <Text>
+                            confira os lanches da cantina:
+                        </Text>
+                    </Box>
+
+                <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
+
+
+                    <Box mt={70}>
+                        <ListProductCard categoria="CANTEEN"/>    
+                    </Box>                    
+
+                    
+                </DropShadow>
         </View>
     );
 }
--- src/pages/Library/index.tsx
+++ src/pages/Library/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { Text, View } from "@gluestack-ui/themed";
+import { Box, Text, View } from "@gluestack-ui/themed";
 import HeaderWithPoints from "../../components/HeaderWithPoints";
+import DropShadow from "react-native-drop-shadow";
+import ListProductCard from "../../components/ListProductCard";
 
 function Library():JSX.Element{
     return(
         <View>
             <HeaderWithPoints/>
+
+            <Box mb={-50} ml={22} mt={45}>
+                        <Text color="$black" fontWeight={"$bold"} fontSize={40} >
+                            Biblioteca
+                        </Text>
+                        <Text>
+                            confira os livros da biblioteca:
+                        </Text>
+                    </Box>
+
+                <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
+
+
+                    <Box mt={70}>
+                        <ListProductCard categoria="LIBRARY"/>    
+                    </Box>                    
+
+                    
+                </DropShadow>
         </View>
     );
 }
