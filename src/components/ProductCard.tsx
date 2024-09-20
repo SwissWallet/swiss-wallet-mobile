065d2c01eb19b232232856f54045ed56ea7c35f6
VictorLapaDev <victorlucaslapa@gmail.com>
2024-09-19 21:35:18 -0300

    fix: product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -67,29 +67,6 @@ function ProductCard({categoria}: PropsProductCard):JSX.Element{
                     </Box>
                 </Box>
 
-                <Box mt={30} mb={10}>
-
-                    <Box justifyContent="center" alignItems="center">
-                        <Image source={(`data:image/jpeg;base64,${products[0].image}`)} alt="camisa" width={250} height={330}/> 
-                    </Box>
-                    
-                    <Box borderRadius={10} bgColor="#2A2A2A" mr={30} ml={30} height={80} justifyContent="center" mt={-25}>
-
-                        <HStack justifyContent="space-evenly" alignItems="center">
-                            <Box> 
-                                <Text color="$white" fontSize={18} fontWeight={"$bold"}>{products[0].name}</Text>
-                                <Text color="$white" fontSize={17} mt={5}>{products[0].value} Pontos</Text>
-                            </Box>
-
-                            <TouchableOpacity onPress={() => favorite({id: products[0].id})}>
-                                <Box bgColor="#C40601" borderRadius={20} width={40} height={40} justifyContent="center" alignItems="center">
-                                    <Icon as={Heart}  color="#fff" size="xl"/>
-                                </Box>
-                            </TouchableOpacity>
-                        </HStack>
-
-                    </Box>
-                </Box>
             </View>
             
             )
