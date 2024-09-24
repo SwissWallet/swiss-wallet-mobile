72de8f11bde4a8c0962c35361bdc5fe07ffad36d
henriquepablo <pablo269704@gmail.com>
2024-09-24 20:00:25 -0300

    feat: adjusting screen size

--- src/pages/AccountSettings/index.tsx
+++ src/pages/AccountSettings/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Box, Text, Input, InputField } from "@gluestack-ui/themed";
+import { View, Box, Text, Input, InputField, ScrollView } from "@gluestack-ui/themed";
 import HeaderWithoutPoints from "../../components/HeaderWithoutPoints";
 import Titlle from "../../components/Title";
 import { TouchableOpacity } from "react-native";
@@ -16,53 +16,54 @@ function AccountSettings(): JSX.Element {
     const user = useSelector((state: any) => state.user.value);
 
     return (
-        <View>
-            <HeaderWithoutPoints />
-            <Box ml={22} mr={22}>
-                <Text color="#000" fontWeight="bold" fontSize={32} marginTop={40}>Conta</Text>
-                <Text color="#000" mt={5}>Informações da conta</Text>
+        <ScrollView flex={1} showsVerticalScrollIndicator={false}>
+            <View>
+                <HeaderWithoutPoints />
+                <Box ml={22} mr={22}>
+                    <Text color="#000" fontWeight="bold" fontSize={32} marginTop={40}>Conta</Text>
+                    <Text color="#000" mt={5}>Informações da conta</Text>
 
 
-                <Box>
-                    <Text fontWeight="$bold" color="#000" fontSize={17} mt={39}>Nome</Text>
-                    <Input mt={7} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6"  isDisabled>
-                        <InputField color="#000" fontWeight="bold" value={user.user.name} />
-                    </Input>
-                    <Text fontWeight="$bold" color="#000" fontSize={17} mt={28}>E-mail</Text>
+                    <Box>
+                        <Text fontWeight="$bold" color="#000" fontSize={17} mt={39}>Nome</Text>
+                        <Input mt={7} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" isDisabled>
+                            <InputField color="#000" fontWeight="bold" value={user.user.name} />
+                        </Input>
+                        <Text fontWeight="$bold" color="#000" fontSize={17} mt={28}>E-mail</Text>
 
-                    <Input mt={7} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6"  isDisabled>
-                        <InputField color="#000" fontWeight="bold" value={user.user.username} />
-                    </Input>
+                        <Input mt={7} bgColor="#C6C6C6" opacity={0.36} borderRadius={8} height={42} $focus-borderColor="#C6C6C6" isDisabled>
+                            <InputField color="#000" fontWeight="bold" value={user.user.username} />
+                        </Input>
 
+                    </Box>
+                    <Box>
+
+                        <DropShadow
+                            style={{ shadowColor: '#000', shadowOffset: { width: 4, height: 5 }, shadowRadius: 2, shadowOpacity: 0.5 }}>
+                            <TouchableOpacity onPress={() => navigation.navigate("ChangingPassword" as never)}>
+                                <Box bgColor="#9A1915" mt={45} alignItems="center" justifyContent="center" height={45} borderRadius={10}>
+                                    <Text color="#fff" fontWeight="$bold" accessible accessibilityLabel="Alterar senha, botão">
+                                        Alterar senha
+                                    </Text>
+                                </Box>
+
+                            </TouchableOpacity>
+                        </DropShadow>
+
+                        <DropShadow
+                            style={{ shadowColor: '#000', shadowOffset: { width: 4, height: 5 }, shadowRadius: 2, shadowOpacity: 0.5 }}>
+                            <TouchableOpacity onPress={() => navigation.navigate("ChangeAdress" as never)}>
+                                <Box bgColor="#9A1915" mt={35} alignItems="center" justifyContent="center" height={45} borderRadius={10} mb={20}>
+                                    <Text color="#fff" fontWeight="$bold" accessible accessibilityLabel="Aterar endereço, botão">
+                                        Alterar endereço
+                                    </Text>
+                                </Box>
+                            </TouchableOpacity>
+                        </DropShadow>
+                    </Box>
                 </Box>
-                <Box>
-
-                    <DropShadow
-                        style={{ shadowColor: '#000', shadowOffset: { width: 4, height: 5 }, shadowRadius: 2, shadowOpacity: 0.5 }}>
-                        <TouchableOpacity onPress={() => navigation.navigate("ChangingPassword" as never)}>
-                            <Box bgColor="#9A1915" mt={45} alignItems="center" justifyContent="center" height={45} borderRadius={10}>
-                                <Text color="#fff" fontWeight="$bold" accessible accessibilityLabel="Alterar senha, botão">
-                                    Alterar senha
-                                </Text>
-                            </Box>
-
-                        </TouchableOpacity>
-                    </DropShadow>
-
-                    <DropShadow
-                        style={{ shadowColor: '#000', shadowOffset: { width: 4, height: 5 }, shadowRadius: 2, shadowOpacity: 0.5 }}>
-                        <TouchableOpacity onPress={() => navigation.navigate("ChangeAdress" as never)}>
-                            <Box bgColor="#9A1915" mt={35} alignItems="center" justifyContent="center" height={45} borderRadius={10}>
-                                <Text color="#fff" fontWeight="$bold" accessible accessibilityLabel="Aterar endereço, botão">
-                                    Alterar endereço
-                                </Text>
-                            </Box>
-                        </TouchableOpacity>
-                    </DropShadow>
-                </Box>
-            </Box>
-        </View>
-
+            </View>
+        </ScrollView>
 
     )
 }
--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import React from "react";
-import { Box, Center, HStack, Icon, Text, View } from "@gluestack-ui/themed";
+import { Box, Center, HStack, Icon, ScrollView, Text, View } from "@gluestack-ui/themed";
 import HeaderWithoutPoints from "../../components/HeaderWithoutPoints";
 import Titlle from "../../components/Title";
 import DropShadow from "react-native-drop-shadow";
@@ -14,122 +14,124 @@ import { isLoged } from "../../redux/reducers/userLoged";
 import { user } from "../../redux/reducers/user";
 import api from "../../service/api";
 
-function Settings():JSX.Element{
+function Settings(): JSX.Element {
 
     const navigation = useNavigation();
     const dispatch = useDispatch();
 
     async function handleSigUp() {
         await AsyncStorage.clear()
-        .then(() => {
-            dispatch(isLoged(false));
-            dispatch(user({}));
-        })
-        .catch(er => console.log(er));
+            .then(() => {
+                dispatch(isLoged(false));
+                dispatch(user({}));
+            })
+            .catch(er => console.log(er));
     }
 
     async function handelDeleteAccount() {
         await api.delete('users')
-        .then(async(json) => {
-            await AsyncStorage.clear()
-            .then(() => {
-                dispatch(isLoged(false));
-                dispatch(user({}));
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-            return Alert.alert('Algo inesperado aconteceu', 'Conta não apagada');
-        })
+            .then(async (json) => {
+                await AsyncStorage.clear()
+                    .then(() => {
+                        dispatch(isLoged(false));
+                        dispatch(user({}));
+                    });
+            })
+            .catch((err) => {
+                console.log(err);
+                return Alert.alert('Algo inesperado aconteceu', 'Conta não apagada');
+            })
     }
 
-    return(
-        <View>
-            <HeaderWithoutPoints />
-            <Titlle name="Configurações da Conta"/>
+    return (
+        <ScrollView flex={1} showsVerticalScrollIndicator={false}>
+            <View>
+                <HeaderWithoutPoints />
+                <Titlle name="Configurações da Conta" />
 
-            <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
-                <Center mt={20}>
-                    <Box bgColor="#fff" height={350} width={260} borderRadius={10}>
-                        
-                        <Box ml={22} mt={20} mr={22}>
-                        
-                            <HStack space="sm" mb={5}>
-                                <TouchableOpacity>
-                                    <Icon as={HelpCircle} size="xl"/>
-                                </TouchableOpacity>
-                                <Text color="#000" fontWeight="$bold">
-                                    Ajuda
-                                </Text>
-                            </HStack>
-                            
-                            <Divider bgColor="#000" mb={5}/>
+                <DropShadow style={{ shadowColor: '#000', shadowOffset: { width: 0, height: 4 }, shadowOpacity: 0.5, shadowRadius: 4 }}>
+                    <Center mt={20}>
+                        <Box bgColor="#fff" height={350} width={260} borderRadius={10}>
 
-                            <HStack space="sm" mb={5} mt={10}>
-                                <TouchableOpacity style={{flexDirection: 'row', gap: 6, alignItems: 'center'}} onPress={() => navigation.navigate('AccountSettings' as never)}>
-                                    <Icon as={IdCard} size={"26"}/>
-                                <Text color="#000" fontWeight="$bold" accessible accessibilityLabel="Configurações da conta, botão">
-                                    Configurações da conta
-                                </Text>
-                                </TouchableOpacity>
-                            </HStack>
+                            <Box ml={22} mt={20} mr={22}>
 
-                            <HStack space="sm" mb={5} mt={10}>
-                                <TouchableOpacity style={{flexDirection: 'row', gap: 6, alignItems: 'center'}}>
-                                    <Icon as={User} size={"26"}/>
-                                <Text color="#000" fontWeight="$bold">
-                                    Termos e Politicas de Privacidade
-                                </Text>
-                                </TouchableOpacity>
-                            </HStack>
+                                <HStack space="sm" mb={5}>
+                                    <TouchableOpacity>
+                                        <Icon as={HelpCircle} size="xl" />
+                                    </TouchableOpacity>
+                                    <Text color="#000" fontWeight="$bold">
+                                        Ajuda
+                                    </Text>
+                                </HStack>
 
-                            <HStack space="sm" mb={5} mt={10}>
-                                <TouchableOpacity style={{flexDirection: 'row', gap: 6, alignItems: 'center'}}>
-                                    <Icon as={LucideMessageSquareWarning} size={"26"}/>
-                                <Text color="#000" fontWeight="$bold">
-                                    Relatar problema
-                                </Text>
-                                </TouchableOpacity>
-                            </HStack>
+                                <Divider bgColor="#000" mb={5} />
 
-                            <Divider bgColor="#000" mb={5}/>
+                                <HStack space="sm" mb={5} mt={10}>
+                                    <TouchableOpacity style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }} onPress={() => navigation.navigate('AccountSettings' as never)}>
+                                        <Icon as={IdCard} size={"26"} />
+                                        <Text color="#000" fontWeight="$bold" accessible accessibilityLabel="Configurações da conta, botão">
+                                            Configurações da conta
+                                        </Text>
+                                    </TouchableOpacity>
+                                </HStack>
 
-                            <Center>
-                                <Text mt={10} color="#000" fontWeight="bold">
-                                    AAPM
-                                </Text>
-                                <Text mt={5}>
-                                    Apoie na gestão da escola no alcance de suas metas e promoção a integração escola-comunidade.
-                                </Text>
-                            </Center>
+                                <HStack space="sm" mb={5} mt={10}>
+                                    <TouchableOpacity style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }}>
+                                        <Icon as={User} size={"26"} />
+                                        <Text color="#000" fontWeight="$bold">
+                                            Termos e Politicas de Privacidade
+                                        </Text>
+                                    </TouchableOpacity>
+                                </HStack>
+
+                                <HStack space="sm" mb={5} mt={10}>
+                                    <TouchableOpacity style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }}>
+                                        <Icon as={LucideMessageSquareWarning} size={"26"} />
+                                        <Text color="#000" fontWeight="$bold">
+                                            Relatar problema
+                                        </Text>
+                                    </TouchableOpacity>
+                                </HStack>
+
+                                <Divider bgColor="#000" mb={5} />
+
+                                <Center>
+                                    <Text mt={10} color="#000" fontWeight="bold">
+                                        AAPM
+                                    </Text>
+                                    <Text mt={5}>
+                                        Apoie na gestão da escola no alcance de suas metas e promoção a integração escola-comunidade.
+                                    </Text>
+                                </Center>
+                            </Box>
                         </Box>
-                    </Box>
-                </Center>
-            </DropShadow>
-
-            <Box ml={22} mr={22} mt={60}>
-                <DropShadow style={{shadowColor: "#000", shadowOffset:{width: 1, height: 4}, shadowOpacity: 0.5, shadowRadius: 2}}>
-                    <TouchableOpacity onPress={handelDeleteAccount}>
-                        <Box bgColor="#fff" borderColor="#E30613" borderWidth={1} borderRadius={5} mb={20} height={45}>
-                            <Text height={40} color="#E30613" fontWeight="$bold" textAlign="center" textAlignVertical="center" accessible accessibilityLabel="Apagar conta, botão">
-                                Apagar Conta
-                            </Text>
-                        </Box>
-                    </TouchableOpacity>
+                    </Center>
                 </DropShadow>
 
-                <DropShadow style={{shadowColor: "#000", shadowOffset:{width: 1, height: 4}, shadowOpacity: 0.5, shadowRadius: 2}}>
-                    <TouchableOpacity onPress={handleSigUp}>
-                        <Box bgColor="#9A1915" borderRadius={5} height={45}>
-                            <Text height={40} color="#fff" fontWeight="$bold" textAlign="center" textAlignVertical="center" accessible accessibilityLabel="Sair do aplicativo, botão">
-                                Sair
-                            </Text>
-                        </Box>
-                    </TouchableOpacity>
-                </DropShadow>
-            </Box>
+                <Box ml={22} mr={22} mt={60}>
+                    <DropShadow style={{ shadowColor: "#000", shadowOffset: { width: 1, height: 4 }, shadowOpacity: 0.5, shadowRadius: 2 }}>
+                        <TouchableOpacity onPress={handelDeleteAccount}>
+                            <Box bgColor="#fff" borderColor="#E30613" borderWidth={1} borderRadius={5} mb={20} height={45}>
+                                <Text height={40} color="#E30613" fontWeight="$bold" textAlign="center" textAlignVertical="center" accessible accessibilityLabel="Apagar conta, botão">
+                                    Apagar Conta
+                                </Text>
+                            </Box>
+                        </TouchableOpacity>
+                    </DropShadow>
 
-        </View>
+                    <DropShadow style={{ shadowColor: "#000", shadowOffset: { width: 1, height: 4 }, shadowOpacity: 0.5, shadowRadius: 2 }}>
+                        <TouchableOpacity onPress={handleSigUp}>
+                            <Box bgColor="#9A1915" borderRadius={5} height={45} mb={20}>
+                                <Text height={40} color="#fff" fontWeight="$bold" textAlign="center" textAlignVertical="center" accessible accessibilityLabel="Sair do aplicativo, botão">
+                                    Sair
+                                </Text>
+                            </Box>
+                        </TouchableOpacity>
+                    </DropShadow>
+                </Box>
+
+            </View>
+        </ScrollView>
     );
 }
 
