20df67377cf24a6d0ecde0bc27b1a3929cad1a02
henriquepablo <pablo269704@gmail.com>
2024-09-23 21:03:39 -0300

    feat: add accesibility header with points

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.15.8:8080/api/v3/'
+    baseURL: 'http://172.16.3.159:8080/api/v3/'
 });
 
 export default api;
