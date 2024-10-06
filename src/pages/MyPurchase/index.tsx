cb75627aca745cf9e491de0cc631be0d3a4bf36f
henriquepablo <pablo269704@gmail.com>
2024-10-06 10:18:36 -0300

    add refresh controll in screen purchase

--- src/pages/MyPurchase/index.tsx
+++ src/pages/MyPurchase/index.tsx
@@ -1,11 +1,13 @@
 import HeaderWithPoints from "../../components/HeaderWithPoints";
 import { useNavigation } from "@react-navigation/native";
-import { Box, FlatList, ScrollView, Text, View } from "@gluestack-ui/themed";
+import { Box, FlatList, RefreshControl, ScrollView, Text, View } from "@gluestack-ui/themed";
 import Titlle from "../../components/Title";
 import DropBox from "../../components/DropsBoxPurchase";
 import api from "../../service/api";
 import { useEffect, useState } from "react";
 import { ActivityIndicator } from "react-native";
+import reloadApp from "../../util/ReloadApp";
+import { useDispatch, useSelector } from "react-redux";
 
 function MyPurchase(): JSX.Element {
 
@@ -13,6 +15,8 @@ function MyPurchase(): JSX.Element {
     const [products, setProducts] = useState();
     const [loading, setLoading] = useState(false);
 
+    const dispatch = useDispatch();
+    const reload = useSelector((state:any) => state.reload.value)
 
     async function loadCarts() {
         setLoading(true);
@@ -30,9 +34,9 @@ function MyPurchase(): JSX.Element {
 
     useEffect(() => {
         loadCarts();
-    }, []);
+    }, [reload]);
 
-    if (loading) {
+    if (loading || reload) {
         return (
             <View>
                 <HeaderWithPoints />
@@ -48,7 +52,9 @@ function MyPurchase(): JSX.Element {
 
     return (
 
-        <ScrollView>
+        <ScrollView showsVerticalScrollIndicator={false} refreshControl={
+            <RefreshControl onRefresh={() => reloadApp(dispatch)} refreshing={reload}/>
+        }>
 
             <View flex={1}>
 
