56be9fbfe28b1bc49ba5d571d59891e7fc00475f
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-08 20:37:39 -0300

    feat: finalized Benefit Page

--- src/pages/Benefits/Benefits.tsx
+++ src/pages/Benefits/Benefits.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Text, View } from "@gluestack-ui/themed";
+import { HStack, Icon, Text, View } from "@gluestack-ui/themed";
 import HeaderWithPoints from "../../components/HeaderWithPoints";
 import Titlle from "../../components/Title";
 import { Box } from "@gluestack-ui/themed";
 import DropShadow from "react-native-drop-shadow";
 import TitleWithoutMargin from "../../components/TitleWithoutMargin";
-import { TouchableOpacity } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { Bold, CircleHelp } from "lucide-react-native";
 
 function Benefits():JSX.Element{
     return(
-        <View>
+        <ScrollView>
             <HeaderWithPoints/>
 
             <Titlle name="Beneficios"/>
@@ -18,22 +19,54 @@ function Benefits():JSX.Element{
 
             <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
 
-                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15}>
+                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10}>
 
                     <TitleWithoutMargin name="Beneficio VT"/>
-                    <Text ml={22} mt={5} mr={22} mb={15}>Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
+                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
                         
                     <TouchableOpacity>
                         <Box justifyContent="center" alignItems="center" mb={10}>
-                            <Box w={200} h={35} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
-                                <Text color="#fff">Solicitar beneficio</Text>
+                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
+                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
                             </Box>
                         </Box>
 			        </TouchableOpacity>
                 </Box>
+
+
+                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10}>
+
+                    <TitleWithoutMargin name="Beneficio VT"/>
+                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
+                        
+                    <TouchableOpacity>
+                        <Box justifyContent="center" alignItems="center" mb={10}>
+                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
+                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
+                            </Box>
+                        </Box>
+                    </TouchableOpacity>
+                </Box>
+
+                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={30}>
+
+                    <TitleWithoutMargin name="Beneficio VT"/>
+                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
+                        
+                    <TouchableOpacity>
+                        <Box justifyContent="center" alignItems="center" mb={10}>
+                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
+                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
+                            </Box>
+                        </Box>
+                    </TouchableOpacity>
+                </Box>
+
             </DropShadow>
 
-        </View>
+            
+
+        </ScrollView>
     );
 }
 
