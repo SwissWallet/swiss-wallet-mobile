ccca89a6762075315824da66487826ffa73a6daa
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-09 22:18:17 -0300

    feat: consuming app

--- src/pages/Benefits/Benefits.tsx
+++ src/pages/Benefits/Benefits.tsx
@@ -16,7 +16,7 @@ function Benefits():JSX.Element{
 
 
     async function getBenefits() {
-        const response = await api.get("/benefit/requests")
+        const response = await api.get("/benefit/actives")
         .then((json) => {
             setBenefits(json.data)
         })
@@ -27,14 +27,13 @@ function Benefits():JSX.Element{
 
 
 
-    async function solicitarBenefits() {
+    async function solicitarBenefits(id: string) {
         
         const response = await api.post('/benefit/requests', {
-            "idBenefit": id,
-            "status": "string"
+            "idBenefit": id
         })
         .then(() => {
-
+            console.log("funfou")
         })
         .catch((error) => {
             console.log(error)
@@ -63,13 +62,14 @@ function Benefits():JSX.Element{
             keyExtractor={(item: any) => item.id}
             renderItem={(item: any) =>
 
+                
                 <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10}>
 
-                    <TitleWithoutMargin name="Beneficio VT"/>
-                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
+                    <TitleWithoutMargin name={item.item.title}/>
+                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">{item.item.description}</Text>
                         
-                    <TouchableOpacity>
-                        <Box justifyContent="center" alignItems="center" mb={10}>
+                    <TouchableOpacity onPress={() => solicitarBenefits(item.item.id) }>
+                        <Box justifyContent="center" alignItems="center" mb={10} >
                             <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
                                 <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
                             </Box>
@@ -78,56 +78,11 @@ function Benefits():JSX.Element{
                 </Box>
                 
                 }
+
             scrollEnabled={false}/>       
             
 
 
-
-
-
-
-                {/* <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10}>
-
-                    <TitleWithoutMargin name="Beneficio VT"/>
-                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
-                        
-                    <TouchableOpacity>
-                        <Box justifyContent="center" alignItems="center" mb={10}>
-                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
-                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
-                            </Box>
-                        </Box>
-			        </TouchableOpacity>
-                </Box>
-
-
-                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10}>
-
-                    <TitleWithoutMargin name="Beneficio VT"/>
-                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
-                        
-                    <TouchableOpacity>
-                        <Box justifyContent="center" alignItems="center" mb={10}>
-                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
-                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
-                            </Box>
-                        </Box>
-                    </TouchableOpacity>
-                </Box>
-
-                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={30}>
-
-                    <TitleWithoutMargin name="Beneficio VT"/>
-                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">Beneficio no qual a AAPM ajuda o aluno que não possui condições de se dirigir ao senai, de forma que ele não precise pagar para percorrer o caminho de casa até o curso.</Text>
-                        
-                    <TouchableOpacity>
-                        <Box justifyContent="center" alignItems="center" mb={10}>
-                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
-                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
-                            </Box>
-                        </Box>
-                    </TouchableOpacity>
-                </Box> */}
 
             </DropShadow>
 
