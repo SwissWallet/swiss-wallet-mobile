ca95ed541b5191339c2ae3242803ec4a56082f89
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-10 22:28:12 -0300

    Feat: consuming api

--- src/pages/Benefits/Benefits.tsx
+++ src/pages/Benefits/Benefits.tsx
@@ -5,7 +5,7 @@ import Titlle from "../../components/Title";
 import { Box } from "@gluestack-ui/themed";
 import DropShadow from "react-native-drop-shadow";
 import TitleWithoutMargin from "../../components/TitleWithoutMargin";
-import { ScrollView, TouchableOpacity } from "react-native";
+import { Alert, ScrollView, TouchableOpacity } from "react-native";
 import { Bold, CircleHelp } from "lucide-react-native";
 import api from "../../service/api";
 
@@ -13,19 +13,19 @@ function Benefits():JSX.Element{
 
 
     const[benefits, setBenefits] = useState([]);
+    const[status, setStatus] = useState([]);
 
-
-    async function getBenefits() {
-        const response = await api.get("/benefit/actives")
+    async function benefitStatus() {
+        const response = await api.get("/benefit/requests/current")
         .then((json) => {
-            setBenefits(json.data)
+            setBenefits(json.data.activeResponseDtos);
+            setStatus(json.data.reqResponseDtos)
+            // console.log(json.data.reqResponseDtos)
         })
         .catch((error) => {
             console.log(error)
         })
     }
-
-
 
     async function solicitarBenefits(id: string) {
         
@@ -33,7 +33,7 @@ function Benefits():JSX.Element{
             "idBenefit": id
         })
         .then(() => {
-            console.log("funfou")
+            return Alert.alert("O beneficio foi solicitado")
         })
         .catch((error) => {
             console.log(error)
@@ -42,11 +42,11 @@ function Benefits():JSX.Element{
 
 
     useEffect(() => {
-        getBenefits();
+        benefitStatus();
     }, []);
 
     return(
-        <ScrollView>
+        <ScrollView showsVerticalScrollIndicator={false}>
             <HeaderWithPoints/>
 
             <Titlle name="Beneficios"/>
@@ -56,20 +56,20 @@ function Benefits():JSX.Element{
             <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
 
 
-
+            <Titlle name="Disponiveis"></Titlle>
             <FlatList
             data={benefits}
             keyExtractor={(item: any) => item.id}
-            renderItem={(item: any) =>
+            renderItem={(item: any) => 
 
-                
-                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10}>
+               
+                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10} padding={20}>
 
                     <TitleWithoutMargin name={item.item.title}/>
-                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify">{item.item.description}</Text>
+                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify" w={250}>{item.item.description}</Text>
                         
-                    <TouchableOpacity onPress={() => solicitarBenefits(item.item.id) }>
-                        <Box justifyContent="center" alignItems="center" mb={10} >
+                    <TouchableOpacity onPress={() => solicitarBenefits(item.item.id)}>
+                        <Box justifyContent="center" alignItems="center" mb={10}>
                             <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
                                 <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
                             </Box>
@@ -79,8 +79,30 @@ function Benefits():JSX.Element{
                 
                 }
 
-            scrollEnabled={false}/>       
+            scrollEnabled={true}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}/>       
             
+
+            <Titlle name="Pendente"></Titlle>
+            <FlatList
+            data={status}
+            keyExtractor={(item: any) => item.id}
+            renderItem={(item: any) => 
+
+               
+                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10} padding={20}>
+
+                    <TitleWithoutMargin name={item.item.benefitActive.title}/>
+                    <Text ml={22} mt={5} mr={22}  padding={5} textAlign="justify" w={250}>{item.item.benefitActive.description}</Text>
+                        
+                </Box>
+                
+                }
+
+            scrollEnabled={true}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}/>   
 
 
 
