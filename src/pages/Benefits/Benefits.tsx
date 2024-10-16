6466fea6234a224ed3c11fa3ef4964bf94c19a1f
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-16 20:23:58 -0300

    update: benefits page finallized

--- src/pages/Benefits/Benefits.tsx
+++ src/pages/Benefits/Benefits.tsx
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, HStack, Icon, Text, View } from "@gluestack-ui/themed";
+import { FlatList, Text} from "@gluestack-ui/themed";
 import HeaderWithPoints from "../../components/HeaderWithPoints";
 import Titlle from "../../components/Title";
 import { Box } from "@gluestack-ui/themed";
 import DropShadow from "react-native-drop-shadow";
-import TitleWithoutMargin from "../../components/TitleWithoutMargin";
 import { Alert, ScrollView, TouchableOpacity } from "react-native";
-import { Bold, CircleHelp } from "lucide-react-native";
 import api from "../../service/api";
+import { useIsFocused } from "@react-navigation/native";
 
 function Benefits():JSX.Element{
 
-
     const[benefits, setBenefits] = useState([]);
     const[status, setStatus] = useState([]);
+
+    const focus = useIsFocused();
 
     async function benefitStatus() {
         const response = await api.get("/benefit/requests/current")
         .then((json) => {
             setBenefits(json.data.activeResponseDtos);
-            setStatus(json.data.reqResponseDtos)
-            // console.log(json.data.reqResponseDtos)
+            setStatus(json.data.reqResponseDtos);
         })
         .catch((error) => {
             console.log(error)
@@ -33,17 +32,18 @@ function Benefits():JSX.Element{
             "idBenefit": id
         })
         .then(() => {
-            return Alert.alert("O beneficio foi solicitado")
+            return Alert.alert("O beneficio foi solicitado");
         })
         .catch((error) => {
-            console.log(error)
+            console.log(error);
         })
-    }
 
+        benefitStatus();
+    }
 
     useEffect(() => {
         benefitStatus();
-    }, []);
+    }, [focus]);
 
     return(
         <ScrollView showsVerticalScrollIndicator={false}>
@@ -52,63 +52,63 @@ function Benefits():JSX.Element{
             <Titlle name="Beneficios"/>
             <Text ml={22} width={300}>Confira a lista de beneficios de assinantes da AAPM</Text>
 
-
             <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
+                <FlatList
+                display={benefits.length == 0 ? 'none' : 'flex'}
+                data={benefits}
+                keyExtractor={(item: any) => item.id}
+                renderItem={(item: any) => 
 
+                
+                    <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={20} padding={20}>
 
-            <Titlle name="Disponiveis"></Titlle>
-            <FlatList
-            data={benefits}
-            keyExtractor={(item: any) => item.id}
-            renderItem={(item: any) => 
-
-               
-                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10} padding={20}>
-
-                    <TitleWithoutMargin name={item.item.title}/>
-                    <Text ml={22} mt={5} mr={22} mb={15} padding={5} textAlign="justify" w={250}>{item.item.description}</Text>
-                        
-                    <TouchableOpacity onPress={() => solicitarBenefits(item.item.id)}>
-                        <Box justifyContent="center" alignItems="center" mb={10}>
-                            <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
-                                <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
+                        <Text color="#000" fontWeight="$bold" fontSize={29} mb={5}>{item.item.title}</Text>
+                        <Text  mt={5} mb={15} padding={5}  w={250}>{item.item.description}</Text>
+                            
+                        <TouchableOpacity onPress={() => solicitarBenefits(item.item.id)}>
+                            <Box justifyContent="center" alignItems="center" mb={10}>
+                                <Box w={200} h={40} bgColor="#C40601" alignItems="center" justifyContent="center" borderRadius={5}>
+                                    <Text color="#fff" fontWeight={"$bold"}>Solicitar beneficio</Text>
+                                </Box>
                             </Box>
-                        </Box>
-                    </TouchableOpacity>
-                </Box>
-                
-                }
+                        </TouchableOpacity>
+                    </Box>
+                    
+                    }
 
-            scrollEnabled={true}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}/>       
-            
-
-            <Titlle name="Pendente"></Titlle>
-            <FlatList
-            data={status}
-            keyExtractor={(item: any) => item.id}
-            renderItem={(item: any) => 
-
-               
-                <Box mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={10} padding={20}>
-
-                    <TitleWithoutMargin name={item.item.benefitActive.title}/>
-                    <Text ml={22} mt={5} mr={22}  padding={5} textAlign="justify" w={250}>{item.item.benefitActive.description}</Text>
-                        
-                </Box>
-                
-                }
-
-            scrollEnabled={true}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}/>   
-
-
-
+                scrollEnabled={true}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}/>       
             </DropShadow>
 
-            
+
+            <Text ml={25} color="#000" fontWeight="$bold" fontSize={32} mb={5} mt={20}>{status.length == 0 ? '' : 'Solicitados'}</Text>
+
+            <DropShadow style={{shadowColor: '#000', shadowOffset: {width: 0, height: 4}, shadowOpacity: 0.5, shadowRadius: 4}}>
+                <FlatList
+                data={status}
+                keyExtractor={(item: any) => item.id}
+                renderItem={(item: any) => 
+
+                
+                    <Box position="relative" mr={22} ml={22} borderRadius={10} bg="#fff" mt={15} mb={20} padding={20}>
+                        <Text color="#000" fontWeight="$bold" fontSize={26} mb={5}>{item.item.benefitActive.title}</Text>
+                        <Text mt={5} w={250}>{item.item.benefitActive.description}</Text>
+                        
+                        
+                            <Text ml={22} mt={5} color="#000" fontWeight={'$semibold'} position="absolute" bottom={10} right={15}>{item.item.status}</Text>
+                        
+                    </Box>
+                    
+                }
+
+                scrollEnabled={true}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                pagingEnabled
+                />   
+
+            </DropShadow>
 
         </ScrollView>
     );
